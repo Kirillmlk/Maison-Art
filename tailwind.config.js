b98955da7d6ddfea05836b276d95a60f42ad3cda
Kirillmlk <kostakov.kiirill@yandex.by>
2025-12-30 16:14:31 +0300

    feat: add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Georgia', 'Times New Roman', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
