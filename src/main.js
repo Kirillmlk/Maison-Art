ff7407acf25a11492e583520c74b87030564add4
Kirillmlk <kostakov.kiirill@yandex.by>
2025-12-30 17:37:06 +0300

    feat: add catalog component

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 import './input.css'
 
+window.navigateTo = function(page) {
+  if (page === 'catalog') {
+    window.location.href = '/catalog.html'
+  } else if (page === 'cabinet') {
+    console.log('Navigate to cabinet')
+  } else if (page === 'home') {
+    window.location.href = '/'
+  }
+}
+
+window.toggleCart = function() {
+  console.log('Toggle cart')
+}
+
+window.openProduct = function(productId) {
+  console.log('Open product:', productId)
+}
+
