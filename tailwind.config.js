dc4828501c82dc70d76101450ff88f759761b850
Kirillmlk <kostakov.kiirill@yandex.by>
2026-01-05 10:49:15 +0300

    feat: add gitignore and font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        serif: ['Georgia', 'Times New Roman', 'serif'],
+        serif: ['Cormorant Garamond', 'serif'],
       },
     },
   },
