47946ab12ab1d7b05553ad84f3935a376c233874
Kirillmlk <kostakov.kiirill@yandex.by>
2026-01-05 15:06:13 +0300

    feat: add vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        catalog: './catalog.html',
+        product: './product-aurelius.html',
+        checkout: './checkout.html',
+        profile: './profile.html',
+      },
+    },
+  },
 })
 
